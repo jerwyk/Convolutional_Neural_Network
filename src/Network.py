9bc26bb5dc654a1a203e304cbbbb567db70ee949
jerwyk <jerwyk0526@gmail.com>
2019-07-10 17:34:23 -0400

    commit

--- src/Network.py
+++ src/Network.py
@@ -4,10 +4,10 @@ import Neuron
 
 class Network:
     
-    def __init__(self, size):
+    def __init__(self, size, layers = ['sigmoid']):
         self.layers_num = len(size)
         self.size = size
-        self.neuron = self.Create_Neurons()
+        self.neuron = self.Create_Neurons(layers)
         #Each element k in the array is a matrix for the weights for the kth layer
         #Each row i the matrix is the weight from all the neurons in the kth layer to the ith neuron in the k+1 layer
         self.weight = [np.random.randn(y, x) for x, y in zip(size[:-1], size[1:])]
@@ -15,15 +15,23 @@ class Network:
         #the input layer has no biases
         self.bias = [np.random.randn(y, 1) for y in size[1:]]
 
-    def Create_Neurons(self):
-        neuro_net = []
+    def Create_Neurons(self, layer_list):
+        layer = []
         for i in range(self.layers_num):
-            layer = []
-            for i in self.size[i]:
-                layer.append(Neuron.Sigmoid_Neuron())
+            if(layer_list[i] == "sigmoid"):
+                layer.append(Neuron.Sigmoid_Neuron)
+        
+        return layer
 
-    def Feed_Forward(self, input):
+    #feed the input data a through the network and returns the prediction
+    def Feed_Forward(self, a):
         for i in range(self.layers_num):
+            a = self.neuron[i].output(a, self.weight[i], self.bias)
+        
+        return a
 
+    def GSD():
+
+    
 
     
--- src/Neuron.py
+++ src/Neuron.py
@@ -1,5 +1,14 @@
 import numpy as np
 
-class Sigmoid_Neuron:
+class Neurons:
+    def output(input, weight, bias):
+        pass
+
+class Sigmoid_Neuron(Neurons):
     def __init__(self):
         pass
+
+    def output(input, weight, bias):
+        z = np.dot(weight, input) + bias
+
+        return 1.0/(1.0 + np.exp(-z))
